7fca5adc488c31fc10720891ce023622d1c2e372
utkarshagarwal01 <coolguy.agarwal365@gmail.com>
2017-04-12 23:51:48 +0530

    Make connection commitable

--- admin_test.php
+++ admin_test.php
@@ -1,9 +1,10 @@
 <?php  
+require_once('../dblogincreds.php');
 if(!isset($_POST["submit"]))
 {
-header("Location: admin_login.html");	
+header("Location: admin_login.php");	
 }
-$connection = mysqli_connect("localhost","root","kamalsonal32","hostel_allotment");
+$connection = mysqli_connect("localhost",$dbuser,$dbpass,"hostel_allotment");
 $username = $_POST["admin_id"];
 $password = $_POST["password"];
 $query = "Select password from admin where admin_id = '{$username}'";
@@ -18,6 +19,8 @@ $row = mysqli_fetch_assoc($result);
 
 if($row["password"] === $password)
 {
+	session_start();
+	$_SESSION["username"] = $username;
 	header("Location: admin_select.html");
 }
 else
