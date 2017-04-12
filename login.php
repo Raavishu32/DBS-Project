2af473078a33e5ced4e8a5e6f658ef936a7e426b
utkarshagarwal01 <coolguy.agarwal365@gmail.com>
2017-04-13 03:06:23 +0530

    File name changed

--- login.php
+++ login.php
@@ -1,3 +1,14 @@
+<?php
+$message = "";
+if ($_GET)
+{
+	if($_GET["password"] == "wrong")
+	{
+		$message = "Username or Password was wrong!.";
+	}
+} 
+
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,11 +25,14 @@
 	<div class="register_page2">
         <div class="form2">
             <h2>Login</h2>
-            <form method="post" action="#" id="register" class="register">
+            <form method="post" action="login_test.php" id="register" class="register">
                	<input type="value" name="reg_no" placeholder="Registration Number"/>
                 <input type="password" name="password" placeholder="Password">
-                <button>Login</button>
+                <button name="submit" value="Submit">Login</button>
             </form>
+            <div style = 'color:red;font-style:italic;margin:10px'>
+            <?=$message; ?>
+            </div>
         </div>
     </div>
 </body>
